4129614ddf4d5bdf0942bc86328fe424a908079b
Scott Graham <scott.github@h4ck3r.net>
2025-06-18 21:11:18 -0700

    some explanation in promote

--- mem.c
+++ mem.c
@@ -9,43 +9,58 @@ typedef struct Slot Slot;
 // Where possible, turns alloca'd slots into temporaries (which are more
 // amendable to optimization that storing/loading into the stack repeatedly).
 void promote(Fn* fn) {
-  Blk* b;
-  Ins *i, *l;
-  Tmp* t;
-  Use *u, *ue;
-  int s, k;
-
   /* promote uniform stack slots to temporaries */
-  b = fn->start;
-  for (i = b->ins; i < &b->ins[b->nins]; i++) {
-    if (Oalloc > i->op || i->op > Oalloc1) {
+
+  // Only processing the instructions in the entry block of the function, and
+  // only handling the OallocN instructions. I think this is a future
+  // optimization opportunity; if the allocas aren't in the entry block, then we
+  // can't trivially know they're not in a loop, etc. so they don't get
+  // promoted. But this means that even a trivial empty block like:
+  //   function w $func() {
+  //   @abc
+  //   @realentry
+  //       %thing =l alloc8, 8
+  //       storel 123, %thing
+  //   @end
+  //       ret 0
+  //   }
+  // means the promotion of %thing won't happen, and as a result the pointless
+  // store will make it into the final output.
+  Blk* b = fn->start;
+  for (Ins* i = b->ins; i < &b->ins[b->nins]; i++) {
+    if (i->op < Oalloc || i->op > Oalloc1) {
       continue;
     }
     /* specific to NAlign == 3 */
     assert(rtype(i->to) == RTmp);
-    t = &fn->tmp[i->to.val];
+    Tmp* t = &fn->tmp[i->to.val];
     if (t->ndef != 1) {
       goto Skip;
     }
-    k = -1;
-    s = -1;
-    for (u = t->use; u < &t->use[t->nuse]; u++) {
+    // For each use of the stack slot, if it's used only by load and store
+    // instructions (not phis, jumps, or any arithmetic, etc.) and the
+    // load/store are always the same size, then it can be promoted to a
+    // temporary. (Some cases that aren't optimized here can get handled later
+    // in load elimination and slot coalescing though.)
+    int k = -1;
+    int s = -1;
+    for (Use* u = t->use; u < &t->use[t->nuse]; u++) {
       if (u->type != UIns) {
         goto Skip;
       }
-      l = u->u.ins;
-      if (isload(l->op)) {
-        if (s == -1 || s == loadsz(l)) {
-          s = loadsz(l);
+      Ins* ins = u->u.ins;
+      if (isload(ins->op)) {
+        if (s == -1 || s == loadsz(ins)) {
+          s = loadsz(ins);
           continue;
         }
       }
-      if (isstore(l->op)) {
-        if (req(i->to, l->arg[1]) && !req(i->to, l->arg[0])) {
-          if (s == -1 || s == storesz(l)) {
-            if (k == -1 || k == optab[l->op].argcls[0][0]) {
-              s = storesz(l);
-              k = optab[l->op].argcls[0][0];
+      if (isstore(ins->op)) {
+        if (req(i->to, ins->arg[1]) && !req(i->to, ins->arg[0])) {
+          if (s == -1 || s == storesz(ins)) {
+            if (k == -1 || k == optab[ins->op].argcls[0][0]) {
+              s = storesz(ins);
+              k = optab[ins->op].argcls[0][0];
               continue;
             }
           }
@@ -56,24 +71,24 @@ void promote(Fn* fn) {
     /* get rid of the alloc and replace uses */
     *i = (Ins){.op = Onop};
     t->ndef--;
-    ue = &t->use[t->nuse];
-    for (u = t->use; u != ue; u++) {
-      l = u->u.ins;
-      if (isstore(l->op)) {
-        l->cls = k;
-        l->op = Ocopy;
-        l->to = l->arg[1];
-        l->arg[1] = R;
+    Use* ue = &t->use[t->nuse];
+    for (Use* u = t->use; u != ue; u++) {
+      Ins* ins = u->u.ins;
+      if (isstore(ins->op)) {
+        ins->cls = k;
+        ins->op = Ocopy;
+        ins->to = ins->arg[1];
+        ins->arg[1] = R;
         t->nuse--;
         t->ndef++;
       } else {
         if (k == -1) {
           err("slot %%%s is read but never stored to",
-              fn->tmp[l->arg[0].val].name);
+              fn->tmp[ins->arg[0].val].name);
         }
         /* try to turn loads into copies so we
          * can eliminate them later */
-        switch (l->op) {
+        switch (ins->op) {
           case Oloadsw:
           case Oloaduw:
             if (k == Kl) {
@@ -81,15 +96,15 @@ void promote(Fn* fn) {
             }
             /* fall through */
           case Oload:
-            if (KBASE(k) != KBASE(l->cls)) {
-              l->op = Ocast;
+            if (KBASE(k) != KBASE(ins->cls)) {
+              ins->op = Ocast;
             } else {
-              l->op = Ocopy;
+              ins->op = Ocopy;
             }
             break;
           default:
           Extend:
-            l->op = Oextsb + (l->op - Oloadsb);
+            ins->op = Oextsb + (ins->op - Oloadsb);
             break;
         }
       }
